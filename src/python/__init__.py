155a9381f01a0ade9964ca7562eefdbca28e5a94
Arnizack <42911677+Arnizack@users.noreply.github.com>
2018-09-17 22:08:06 +0200

    CMake Verbesserung mit Path Variablen für Blender/python/addons

---
+++ src/python/__init__.py
@@ -0,0 +1,15 @@
+#    Addon info
+
+bl_info = {
+    'name': 'JPCEngine',
+    'author': 'Jeremy Funk, Peer Drescher & Christian Libner',
+    'location': 'Rendertab',
+    'category': 'Render'
+    }
+
+import bpy
+def register():
+	pass
+def unregister():
+	pass
+	
