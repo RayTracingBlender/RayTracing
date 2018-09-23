94f9d96d20d3208b6dc2386dbd833f836fc3f050
Christian Libner <42911677+Arnizack@users.noreply.github.com>
2018-09-23 16:30:04 +0200

    Release lib von Python wird immer importiert

--- src/core/include.h
+++ src/core/include.h
@@ -1,2 +1,11 @@
+// Auch wenn man Debugt wird die Release lib von Python importiert
+#ifdef _DEBUG
+#define _DEBUG_WAS_DEFINED 1
+#undef _DEBUG
+#endif
+
 #include <Python.h>
 
+#ifdef _DEBUG_WAS_DEFINED
+#define _DEBUG 1
+#endif
